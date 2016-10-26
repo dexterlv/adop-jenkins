2de4a067d2a5d048023ab7e13ef7a2e34248cbb2
Jakovlevs <dmitrijs.jakovlevs@accenture.com>
2016-10-26 13:57:34 +0300

    gerrit ssh key generation disabled

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -5,7 +5,7 @@ host=$GERRIT_HOST_NAME
 port=$GERRIT_PORT
 username=$GERRIT_JENKINS_USERNAME
 password=$GERRIT_JENKINS_PASSWORD
-nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} -u ${username} -w ${password} &
+# nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} -u ${username} -w ${password} &
 
 echo "skip upgrade wizard step after installation"
 echo "2.7.4" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
